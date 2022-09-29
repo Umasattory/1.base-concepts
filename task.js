f2b1d048d7e98b3d648c6395cb0802d73bb06be4
Умед Сатторов <umasattory@gmail.com>
2022-09-29 18:18:27 +0300

    Correcting homeWork bace-concepts

---
+++ task.js
@@ -0,0 +1,50 @@
+"use strict"
+function solveEquation(a, b, c) {
+	let arr, discrimCount, sqr0, sqr1, sqr2;
+	discrimCount = b ** 2 - 4 * a * c;
+	sqr0 = (-b / 2 * a)
+	sqr1 = (-b + Math.sqrt(discrimCount)) / (2 * a)
+	sqr2 = (-b - Math.sqrt(discrimCount)) / (2 * a)
+	if (discrimCount > 0) {
+		arr = [sqr1, sqr2];
+	} else if (discrimCount === 0) {
+		arr = [sqr0];
+	} else {
+		arr = [];
+	}
+	return arr;
+}
+
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+	"use strict"
+
+	if (isNaN(percent)) {
+		return (`Не корректное значение параметра "Процентная ставка" "${percent}"`);
+	}
+	if (isNaN(contribution)) {
+		return(`Не корректное значение параметра "Первоначальный взнос" "${contribution}"`)
+	}
+	if (isNaN(amount)) {
+		return(`Не корректное значение параметра "Общая стоимость" "${amount}"`)
+	}
+
+	let totalAmount, loanBody, monthes, today, monthPay, p;
+
+	//Определение "тела кредита"
+	loanBody = amount - contribution; 
+
+	//Определение количества месяцев
+	today = new Date();
+	monthes = (date.getFullYear() - today.getFullYear()) * 12;
+	monthes -= today.getMonth();
+	monthes += date.getMonth();
+	monthes <= 0 ? 0 : monthes;
+
+	//Определение ежемесячной платы
+	p = percent / (100 * 12);
+	monthPay = loanBody * (p + (p / (((1 + p) ** monthes) - 1)));
+	totalAmount = Math.round((monthPay * monthes)*100)/100;
+	console.log(totalAmount)
+	return totalAmount;
+}	
